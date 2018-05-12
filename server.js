61461d6c90587a4f0083f68f51de9ab94625c239
Glitch (timemicroserv) <none>
2018-05-12 20:24:29 +0000

    ⛸🎠 Checkpoint ./views/index.html:1047756/871 ./public/style.css:1047756/83 ./public/client.js:1047756/1270 ./server.js:1047756/64

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
+  /// 
+  
+  
 });
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
